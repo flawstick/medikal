2afa9021ab4e5eec79df210938ba90483779d8bf
flawstick <amirashak101@gmail.com>
2025-09-11 16:42:55 +0300

    Adding new download route

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export function GET() {
   return NextResponse.redirect(
-    'https://medikal-app.s3.il-central-1.amazonaws.com/app-release.apk',
-    302
+    "https://pub-935a9967c0664658862019699749d4f6.r2.dev/application-197413f4-d05d-4f6f-abd0-105c2e958fad.apk",
+    302,
   );
 }
