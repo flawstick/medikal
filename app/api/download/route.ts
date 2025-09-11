5c4a47c30cab69e76986a3a90eabdd6996e68114
flawstick <amirashak101@gmail.com>
2025-09-12 02:45:36 +0300

    Changing the route to make it simpler

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export function GET() {
   return NextResponse.redirect(
-    "https://pub-935a9967c0664658862019699749d4f6.r2.dev/application-197413f4-d05d-4f6f-abd0-105c2e958fad.apk",
+    "https://pub-935a9967c0664658862019699749d4f6.r2.dev/medikal.apk",
     302,
   );
 }
